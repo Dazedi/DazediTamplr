07f160f19607b671e85be13e7103d780ee13b10f
Dazedi <tommi.vepsa@student.tut.fi>
2015-02-21 15:21:41 +0200

    d

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var models = require('../models');
 
-router.get('/', function(req, res) {
+router.get('/', function(req, res, next) {
   models.User.findAll().then(function(users) {
     res.render('index', {
       host: req.headers.host,
