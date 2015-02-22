ffe200588d2d45b72a0d3d4da1156ed6709af9c8
Dazedi <tommi.vepsa@student.tut.fi>
2015-02-22 03:37:44 +0200

    no auth
    too complicated

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var models = require('../models');
-var auth = require('../auth');
+//var auth = require('../auth');
 
 /*
 function IsAuthenticated(req, res, next){
@@ -22,7 +22,7 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get('/login',auth, function(req, res, next) {
+router.get('/login', function(req, res, next) {
   models.User.findAll().then(function(users) {
     res.render('login', {
       host: req.headers.host,
