ed1032ac866d78e2dadf1dac7d247a6c7ba15063
Dazedi <tommi.vepsa@student.tut.fi>
2015-02-22 03:34:37 +0200

    auth?

--- auth.js
+++ auth.js
@@ -1,13 +1,22 @@
 var auth = require('basic-auth');
 var models = require('../models');
 
-/*
+
 module.exports = function(req, res, next){
   var user = auth(req);
-  models.User.findAll()
+  models.User.findAll().success(function(userlist) {
+  	for(var i=0;i<userlist.length;i++){
+    	if (!user || user.name !== userlist[i].username || user.password !== userlist[i].password) {
+		    res.set('WWW-Authenticate', 'Basic realm="example"');
+		    return res.status(401).send();
+		  }
+  	}
+  });
+
+/*
   if (!user || !users[user.name] || users[user.name].password !== user.pass) {
     res.set('WWW-Authenticate', 'Basic realm="example"');
     return res.status(401).send();
-  }
+  }*/
   return next();
-};*/
+};
